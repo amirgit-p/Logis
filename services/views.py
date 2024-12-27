34b59109e2f57b88149ca7d4864ca067fbd705be
arazkuchaki <161757241+arazkuchaki@users.noreply.github.com>
2024-12-27 17:40:39 +0330

    fixed services/view

--- services/views.py
+++ services/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from .models import Services_details,Services,Features
+from root.models import Testimonials,Fqa
 
-def services (request):
-    return render(request , 'services/services.html')
+def service (request):
+    services = Services.objects.filter(status = True)
+    features = Features.objects.filter(status = True)
+    tester = Testimonials.objects.filter(status = True)
+    fqa = Fqa.objects.all()
+    context = {
+        services: 'services',
+        features: 'features',
+        tester: 'tester',
+        fqa: 'fqa',
+    }
 
-def service_dtail (request):
-    return render(request , 'services/service-dtail.html')
+    return render(request , 'services/services.html',context=context)
+
+def service_detail (request):
+    service = Services_details.objects.filter(default=True)
+    context  = {
+    service :'service',
+    }
+    return render(request , 'services/service-dtail.html',context=context)
