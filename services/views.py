28b343e627d6ad44d8bc564307524598a552c5d0
YVismeh <Yvisme41148@gmail.com>
2024-12-28 21:58:00 +0330

    paginator

--- services/views.py
+++ services/views.py
@@ -1,24 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Services_details,Services,Features
 from root.models import Testimonials,Fqa
+from django.core.paginator import Paginator
 
 def service (request):
+
     services = Services.objects.filter(status = True)
     features = Features.objects.filter(status = True)
     tester = Testimonials.objects.filter(status = True)
     fqa = Fqa.objects.all()
+
+    services_paginate = Paginator(services, 3)
+    first_page = 1
+    last_page = services_paginate.num_pages
+
+    try:
+        page_number = request.GET.get("page")
+        services = services_paginate.get_page(page_number)
+    except:
+        page_number = first_page
+        services = services_paginate.get_page(first_page)
+
     context = {
-        services: 'services',
-        features: 'features',
-        tester: 'tester',
-        fqa: 'fqa',
+        "services" : services,
+        "features" : features,
+        "tester" : tester,
+        "fqa" : fqa,
+        "first" : first_page,
+        "last" : last_page,
     }
 
     return render(request , 'services/services.html',context=context)
 
-def service_detail (request):
-    service = Services_details.objects.filter(default=True)
+def service_detail (request, id):
+    services = get_object_or_404(Services, id=id)
+
     context  = {
-    service :'service',
+    "services" : services,
     }
-    return render(request , 'services/service-dtail.html',context=context)
+
+    return render(request , 'services/service-details.html',context=context)
