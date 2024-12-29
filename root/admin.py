9c6faa4f44b5dda96b96fc278704a8620842ee01
bugnaslZ <bugnaslz83@gmail.com>
2024-12-29 08:56:18 -0800

    quote and contact

--- root/admin.py
+++ root/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User,Agent,CountStars,Job,Fqa,Testimonials
+from .models import User,Agent,CountStars,Job,Fqa,Testimonials,Contact,Quote
 
 
 
@@ -16,4 +16,8 @@ admin.site.register(Job)
 admin.site.register(Fqa)
 
 admin.site.register(Testimonials)
+
+admin.site.register(Contact)
+
+admin.site.register(Quote)
 
--- root/views.py
+++ root/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Agent,Testimonials,Fqa
+from .models import Agent,Testimonials,Fqa,Contact,Quote
 from services.models import Services,Features
+from .forms import Contactform,Quoteform
+from django.contrib import messages
 def home(request):
     services = Services.objects.filter(status = True)
     features = Features.objects.filter(status = True)
@@ -26,4 +28,42 @@ def about(request):
     return render (request, 'root/about.html',context=context) 
 
 def contact(request):
-    return render (request, 'root/contact.html')
+    if request.method == 'POST':
+        form = Contactform(request.POST)
+        if form.is_valid():
+            contact = Contact()
+            contact.name = request.POST.get('name')
+            contact.email = request.POST.get('email')
+            contact.subject = request.POST.get('subject')
+            contact.message = request.POST.get('message')
+            contact.save()
+            messages.add_message(request,messages.SUCCESS,'success')
+            return render (request, 'root/contact.html')
+        else:
+            messages.add_message(request,messages.ERROR,'not send')
+            return render (request, 'root/contact.html')
+
+    else:
+        return render (request, 'root/contact.html')
+    
+def quote(request):
+    if request.method == 'POST':
+        quoteform = Quoteform(request.POST)
+        if quoteform.is_valid():
+            quote = Quote()
+            quote.departure = request.POST.get('departure')
+            quote.delivery = request.POST.get('delivery')
+            quote.weight = request.POST.get('weight')
+            quote.dimensions = request.POST.get('dimensions')
+            quote.name = request.POST.get('name')
+            quote.email = request.POST.get('email')
+            quote.message = request.POST.get('message')
+            quote.save()
+            messages.add_message(request,messages.SUCCESS,'done sent')
+            return render (request,'root/get-a-quote.html')
+        else:
+            messages.add_message(request,messages.ERROR,'not sent')
+            return render (request,'root/get-a-quote.html')
+
+    else:
+        return render (request,'root/get-a-quote.html')
